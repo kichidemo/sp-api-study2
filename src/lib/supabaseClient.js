27d7024d35d10cf2bc999a9da9eccc68c829f708
keiichi <kichi.azure@gmail.com>
2025-04-09 20:49:07 +0900

    adapterをルートに追加

--- src/lib/supabaseClient.js
+++ src/lib/supabaseClient.js
@@ -3,4 +3,5 @@ import { createClient } from '@supabase/supabase-js';
 const supabaseUrl = import.meta.env.PUBLIC_SUPABASE_URL;
 const supabaseAnonKey = import.meta.env.PUBLIC_SUPABASE_ANON_KEY;
 
+console.log(import.meta.env.PUBLIC_SUPABASE_URL);
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
