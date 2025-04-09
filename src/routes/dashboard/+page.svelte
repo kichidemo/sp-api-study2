28c957711755a604a892f848fb358b5f6a426b3a
keiichi <kichi.azure@gmail.com>
2025-04-09 20:29:35 +0900

    ログイン機能追加

---
+++ src/routes/dashboard/+page.svelte
@@ -0,0 +1,19 @@
+<script>
+    import { supabase } from "$lib/supabaseClient";
+
+    let user = '';
+
+    const loadUser = async () => {
+        const { data } = await supabase.auth.getUser()
+        user = data.user
+    }
+
+    loadUser()
+</script>
+
+<h1>ダッシュボード</h1>
+{#if user}
+    <p>ようこそ{user.email}</p>
+{:else}
+    <p>ログインしていません。</p>
+{/if}
---
+++ src/routes/login/+page.svelte
@@ -0,0 +1,20 @@
+<script>
+    import { supabase } from '$lib/supabaseClient';
+
+    let email = '';
+    let password = '';
+
+    const handleLogin = async() => {
+        const { error } = await supabase.auth.signInWithPassword({ email, password});
+        if (error) {
+            alert('ログイン失敗'+ error.message);
+        } else {
+            window.location.href = '/dashboard'
+        }
+    }
+</script>
+
+<h1>ログイン</h1>
+<input type="email" bind:value={email} placeholder="Email" />
+<input type="password" bind:value={password} placeholder="パスワード" />
+<button on:click={handleLogin}>ログイン</button>
